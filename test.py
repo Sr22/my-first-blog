364e79ee0ce296f0f1334092808344570ba88012
Shayla <shayla.rao@gmail.com>
2017-05-13 10:38:39 -0700

    Django girls app

---
+++ test.py
@@ -0,0 +1,14 @@
+print("computer")
+if 4>5:
+	print(">")
+else:
+	print("<")
+def coding():
+	print("hi")
+
+def multiplyer(number,multiplyer):
+	return number*multiplyer
+
+print(multiplyer(5,5))
+
+
